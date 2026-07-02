314d7be46e39ac480d2400a9e9a45b214cab1989
Wandile Shabangu <wandile.shabangu@dlalagroup.com>
2026-07-02 19:58:26 +0200

    feat(app): scaffold Flutter project (tradeiq_app)

---
+++ app/test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('app boots and renders a MaterialApp', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Scaffold()));
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
