d22bbb93ceaf68162701560bd4db310bccbe8968
Wandile Shabangu <wandile.shabangu@dlalagroup.com>
2026-07-03 11:46:38 +0200

    feat(app): add LocationService wrapping geolocator for check-in GPS

---
+++ app/lib/core/location/geolocator_gateway.dart
@@ -0,0 +1,25 @@
+import 'package:geolocator/geolocator.dart';
+
+/// Thin wrapper around the static [Geolocator] calls this app needs, so
+/// [LocationService] can be unit-tested with a fake instead of mocking
+/// geolocator's platform-channel internals.
+abstract class GeolocatorGateway {
+  Future<LocationPermission> checkPermission();
+  Future<LocationPermission> requestPermission();
+  Future<bool> isLocationServiceEnabled();
+  Future<Position> getCurrentPosition();
+}
+
+class RealGeolocatorGateway implements GeolocatorGateway {
+  @override
+  Future<LocationPermission> checkPermission() => Geolocator.checkPermission();
+
+  @override
+  Future<LocationPermission> requestPermission() => Geolocator.requestPermission();
+
+  @override
+  Future<bool> isLocationServiceEnabled() => Geolocator.isLocationServiceEnabled();
+
+  @override
+  Future<Position> getCurrentPosition() => Geolocator.getCurrentPosition();
+}
